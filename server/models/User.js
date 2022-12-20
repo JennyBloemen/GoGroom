65fd076ed3a942534f9f6a44fbf24280f1ac4c8a
kezie14 <kezie.stewart@outlook.com>
2022-12-19 21:57:46 -0700

    models added

---
+++ server/models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
