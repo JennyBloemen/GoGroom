3995f2e8b8a8b065ed2199defc411e3b00ea8cfe
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-21 16:31:50 -0700

    Initial home page code writted

---
+++ client/src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+import ProfileList from '../components/ProfileList';
+
+import { QUERY_PROFILES } from '../utils/queries';
+
+const Home = () => {
+  const { loading, data } = useQuery(QUERY_PROFILES);
+  const profiles = data?.profiles || [];
+
+  return (
+    // we will need to write this to fit out project
+    // <main>
+    //   <div className="flex-row justify-center">
+    //     <div className="col-12 col-md-10 my-3">
+    //       {loading ? (
+    //         <div>Loading...</div>
+    //       ) : (
+    //         <ProfileList
+    //           profiles={profiles}
+    //           title="Here's the current roster of friends..."
+    //         />
+    //       )}
+    //     </div>
+    //   </div>
+    // </main>
+  );
+};
+
+export default Home;
