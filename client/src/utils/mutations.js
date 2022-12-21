3e94287afbbf32c0ac972987e7cb632ce8276780
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-21 16:36:48 -0700

    mutations pulled from class content

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,35 @@
+import { gql } from "@apollo/client";
+
+export const ADD_PROFILE = gql`
+  mutation addProfile($name: String!, $email: String!, $password: String!) {
+    addProfile(name: $name, email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+// export const ADD_SKILL = gql`
+//   mutation addSkill($profileId: ID!, $skill: String!) {
+//     addSkill(profileId: $profileId, skill: $skill) {
+//       _id
+//       name
+//       skills
+//     }
+//   }
+// `;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
