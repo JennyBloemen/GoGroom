e801091e68201b8ede785d561fa4327cdf8a2faa
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-21 16:36:25 -0700

    initial auth.js code written

---
+++ client/src/utils/auth.js
@@ -0,0 +1,38 @@
+//copied from 21/23 MERN JWT Decode
+
+// use this to decode a token and get the user's information out of it
+import decode from "jwt-decode";
+
+// create a new class to instantiate for a user
+class AuthService {
+  // get user data from JSON web token by decoding it
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  // return `true` or `false` if token exists (does not verify if it's expired yet)
+  loggedIn() {
+    const token = this.getToken();
+    return token ? true : false;
+  }
+
+  getToken() {
+    // Retrieves the user token from localStorage
+    return localStorage.getItem("id_token");
+  }
+
+  login(idToken) {
+    // Saves user token to localStorage and reloads the application for logged in status to take effect
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
+
+  logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem("id_token");
+    // this will reload the page and reset the state of the application
+    window.location.reload();
+  }
+}
+
+export default new AuthService();
