6afa9730ac86383bbce8e60ebc119a33a3bc2ec1
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-21 16:37:09 -0700

    query profiles code written

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PROFILES = gql`
+  query allProfiles {
+    profiles {
+      _id
+      name
+      skills
+    }
+  }
+`;
+
+export const QUERY_SINGLE_PROFILE = gql`
+  query singleProfile($profileId: ID!) {
+    profile(profileId: $profileId) {
+      _id
+      name
+      skills
+    }
+  }
+`;
