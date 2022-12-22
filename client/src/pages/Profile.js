c7d5e1407aa06b4ecb898255c66d89c8911c33bb
Emmalee <emilee.wimmer@gmail.com>
2022-12-21 21:13:33 -0700

    adding images and wecontainer code

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -21,17 +21,17 @@ const Profile = () => {
     return <div>Loading...</div>;
   }
   return (
-    <div>
-      <h2 className="card-header">
-        {profile.name}'s friends have endorsed these skills...
-      </h2>
-
-      {profile.skills?.length > 0 && <SkillsList skills={profile.skills} />}
-
-      <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
-        <SkillForm profileId={profile._id} />
-      </div>
-    </div>
+    // <div>
+    //   <h2 className="card-header">
+    //     {profile.name}'s friends have endorsed these skills...
+    //   </h2>
+    //
+    //   {profile.skills?.length > 0 && <SkillsList skills={profile.skills} />}
+    //
+    //   <div className="my-4 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+    //     <SkillForm profileId={profile._id} />
+    //   </div>
+    // </div>
   );
 };
 
