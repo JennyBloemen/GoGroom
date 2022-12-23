99cc7e2d6c742bfee73805ce6669e0e943a55141
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-22 22:19:44 -0700

    removed logo not being used

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 
 import "./App.css";
 
