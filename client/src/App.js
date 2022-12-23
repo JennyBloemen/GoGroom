5a2430ddbfa9196a0dca682fc9ba86b7d2392eab
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-22 20:28:30 -0700

    updated imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
-import logo from "./logo.svg"; //need to add our logo to the assets and update
-import "./App.css";
-//coe from 21/23 src utils app.just not sure we need this code yet
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
 } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import logo from "./logo.svg";
 
-// need to create these pages
+//ask ta if these two are in correct location and if setcontext is needed. It is not in the mini project
+import "./App.css";
+import { setContext } from "@apollo/client/link/context";
+
+import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+// need to create these pages
+// import Scheduling from "./pages/Scheduling";
+// import Services from "./pages/Services";
 
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
 const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
   const token = localStorage.getItem("id_token");
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
@@ -34,8 +30,11 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
 const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
@@ -45,7 +44,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -55,37 +54,26 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
-      {/* <ApolloProvider client={client}>
-      <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
-              <Route 
-                path="/" 
-                element={<Home />} 
-              />
-              <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-              <Route 
-                path="/signup" 
-                element={<Signup />} 
-              />
-              <Route 
-                path="/profiles/:profileId" 
-                element={<Profile />} 
-              />
-            </Routes>
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="flex-column justify-flex-start min-100-vh">
+            <Header />
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/profile/:profileId" element={<Profile />} />
+                {/* <Route path="/services" element={<Services />} />
+                <Route path="/scheduling" element={<Scheduling />} /> */}
+              </Routes>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
-    </ApolloProvider> */}
-      ); }
+        </Router>
+      </ApolloProvider>
     </div>
   );
 }
