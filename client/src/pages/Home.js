2b3dc9ca66b7dd7bb1e761cee4642b78ccf566fc
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-12-22 22:20:02 -0700

    reset commented out jsx

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,32 +11,32 @@ function Home() {
   // temporary code until services are built, need to remove
   const services = ["brush", "wash", "trim"];
   return (
-    <>
-      <h1>test</h1>
-    </>
-    // <div className="homeBody">
-    //   <div className="gifGuy">
-    //     <img src="" alt="dog in bathtub gif" />
-    //   </div>
-    //   <div className="dogBanner">
-    //     <img src="" alt="dog head image" />
-    //     <ul className="dogServiceList">
-    //       {services.map((service) => (
-    //         <li className="dogServiceListItem" key={service.uuid}>
-    //           {`${service.name} ${service.description} (${service.price})`}
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   </div>
-    //   <div className="catBanner">
-    //     <img src="" alt="cat head image" />
-    //     <ul>
-    //       <li>service 1</li>
-    //       <li>service 2</li>
-    //       <li>service 3</li>
-    //     </ul>
-    //   </div>
-    // </div>
+    // <>
+    //   <h1>test</h1>
+    // </>
+    <div className="homeBody">
+      <div className="gifGuy">
+        <img src="" alt="dog in bathtub gif" />
+      </div>
+      <div className="dogBanner">
+        <img src="" alt="dog head image" />
+        <ul className="dogServiceList">
+          {services.map((service) => (
+            <li className="dogServiceListItem" key={service.uuid}>
+              {`${service.name} ${service.description} (${service.price})`}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="catBanner">
+        <img src="" alt="cat head image" />
+        <ul>
+          <li>service 1</li>
+          <li>service 2</li>
+          <li>service 3</li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
