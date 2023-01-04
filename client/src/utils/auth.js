a28b02b18d235d94a75afb743893080448fdee70
Jenny Bloemen <jennyjbloemen@gmail.com>
2023-01-03 20:16:33 -0700

    redirect first attempt

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -24,7 +24,7 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage and reloads the application for logged in status to take effect
     localStorage.setItem("id_token", idToken);
-    window.location.assign("/");
+    window.location.assign("/profile/:id");
   }
 
   logout() {
