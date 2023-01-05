f702b2778bf8eddb3bc9492a5cfe4d28c322c5c0
Jenny Bloemen <jennyjbloemen@gmail.com>
2023-01-04 23:42:56 -0700

    Add imports and commented out code

--- client/src/pages/Scheduling.js
+++ client/src/pages/Scheduling.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
-import DateTimePicker from 'react-datetime-picker';
-import Header from '../components/webContainer.js';
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import DateTimePicker from "react-datetime-picker";
+import Header from "../components/webContainer.js";
 
 // function for Scheduling page
 // function Scheduling() {
@@ -15,74 +15,73 @@ import Header from '../components/webContainer.js';
 //   );
 // }
 
-
 // import ProfileList from '../components/ProfileList';
 
-import { QUERY_PROFILES } from '../utils/queries';
+import { QUERY_PROFILES } from "../utils/queries";
 
 const Scheduling = () => {
-  const { loading, data } = useQuery(QUERY_PROFILES);
-  const profiles = data?.profiles || [];
+  // const { loading, data } = useQuery(QUERY_PROFILES);
+  // const profiles = data?.profiles || [];
 
-return (
-  <div>
-     <Header></Header>
-<div className='homeBody'>
-    <h1>Schedule An Appointment</h1>
-  
-        
-        <form className='makeApt'>
-        <label>
-    Pet's Name:
-    <input type="text" name="name" />
-  </label>
+  const token = window.localStorage.getItem("id_token");
+  const user = jwt_decode(token);
+  const userid = user.data._id;
 
-  <select id='day'>
-  <option value="mon">Monday</option>
-  <option value="tues">Tuesday</option>
-  <option selected value="wed">Wednesday</option>
-  <option value="thurs">Thursday</option>
-  <option value="fri">Friday</option>
-</select>
-<select id='time'>
-  <option value="9-11">9 AM - 11 AM</option>
-  <option value="11-1">11 AM - 1 PM</option>
-  <option defaultValue="1-3">1 PM - 3 PM</option>
-  <option value="3-5">3 PM - 5 PM</option>
-  <option value="5-7">5 PM - 7 PM</option>
-</select>
-<select id='service'>
-  {/* TODO @ ashley map through services and move button if you want */}
+  return (
+    <div>
+      <Header></Header>
+      <div className="homeBody">
+        <h1>Schedule An Appointment</h1>
 
-  <option value="bathAndBrush">Bath And Brush</option>
-  <option value="nailFileAndPrep">Nail File and Prep</option>
-  <option defaultValue="topCat">Top Cat</option>
-  <option value="topCatPlus">Top Cat Plus</option>
-  <option value="bathAndClip">Bath And Clip</option>
-  <option value="nailGrindingAndSanitation">Nail Grinding & Sanitation</option>
-  <option value="topDog">Top Dog</option>
-  <option value="topDogPlus">Top Dog Plus</option>
-  <input type="submit"></input>
+        <form className="makeApt">
+          <label>
+            Pet's Name:
+            <input type="text" name="name" />
+          </label>
 
-</select>
+          <select id="day">
+            <option value="mon">Monday</option>
+            <option value="tues">Tuesday</option>
+            <option selected value="wed">
+              Wednesday
+            </option>
+            <option value="thurs">Thursday</option>
+            <option value="fri">Friday</option>
+          </select>
+          <select id="time">
+            <option value="9-11">9 AM - 11 AM</option>
+            <option value="11-1">11 AM - 1 PM</option>
+            <option defaultValue="1-3">1 PM - 3 PM</option>
+            <option value="3-5">3 PM - 5 PM</option>
+            <option value="5-7">5 PM - 7 PM</option>
+          </select>
+          <select id="service">
+            {/* TODO @ ashley map through services and move button if you want */}
 
-            <select>
-              {/* // day, time, and service */}
-              
-              </select>
-              {/* { loading ? ( <option>Loading...</option> ) : ( <option>Select a service</option> ) } 
+            <option value="bathAndBrush">Bath And Brush</option>
+            <option value="nailFileAndPrep">Nail File and Prep</option>
+            <option defaultValue="topCat">Top Cat</option>
+            <option value="topCatPlus">Top Cat Plus</option>
+            <option value="bathAndClip">Bath And Clip</option>
+            <option value="nailGrindingAndSanitation">
+              Nail Grinding & Sanitation
+            </option>
+            <option value="topDog">Top Dog</option>
+            <option value="topDogPlus">Top Dog Plus</option>
+            <input type="submit"></input>
+          </select>
+
+          <select>{/* // day, time, and service */}</select>
+          {/* { loading ? ( <option>Loading...</option> ) : ( <option>Select a service</option> ) } 
               {scheduling.map((scheduling) => ( 
                 <option key={profiles.uuid} value>
                   {`${scheduling.name} ${scheduling.description} (${scheduling.price})`} */}
-                {/* </option> */}
-              {/* ))} */}
-
-
+          {/* </option> */}
+          {/* ))} */}
         </form>
-    
-  
-</div>
-</div>
-)};
+      </div>
+    </div>
+  );
+};
 
 export default Scheduling;
