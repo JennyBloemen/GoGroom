cf50907b93db9b26da8277f2b45c0c2afda78ac6
kezie14 <kezie.stewart@outlook.com>
2023-01-04 22:00:23 -0700

    added footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Footer() {
+
+    return (
+
+        <footer>
+            Made with LOVE Jenny, Emm, Ash and Kez
+        </footer>
+
+    );
+}
+
+export default Footer
