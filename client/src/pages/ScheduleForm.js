f1da9c7ac7bb7122109bdd66a26bae7f8e9b47e2
Jenny Bloemen <jennyjbloemen@gmail.com>
2023-01-04 23:41:01 -0700

    Created new schedule form, not complete

---
+++ client/src/pages/ScheduleForm.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import Header from "../components/webContainer.js";
+import { ADD_PET } from "../utils/mutations";
+import { QUERY_PETS } from "../utils/queries";
+
+import Auth from "../utils/auth";
+import jwt_decode from "jwt-decode";
+
+const PetForm = () => {
+  const token = window.localStorage.getItem("id_token");
+  const user = jwt_decode(token);
+  const userid = user.data._id;
+
+  const [formState, setFormState] = useState({
+    name: "",
+    breed: "",
+    sex: "",
+    weight: "",
+    age: "",
+    owner: userid,
+  });
+  const [addPet, { error, data }] = useMutation(ADD_PET);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+  var navigate = useNavigate();
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addPet({
+        variables: { ...formState },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+    setFormState({
+      name: "",
+      breed: "",
+      sex: "",
+      weight: "",
+      age: "",
+      owner: userid,
+    });
+  };
+
+  return (
+    <div>
+      <Header></Header>
+      <h3> Add Your Adorable Pet!</h3>
+      <div>
+        <form onSubmit={handleFormSubmit}>
+          <input
+            className="form-input"
+            placeholder="Pet Name"
+            name="name"
+            type="text"
+            value={formState.name}
+            onChange={handleChange}
+          />
+          <input
+            className="form-input"
+            placeholder="Pet Breed"
+            name="breed"
+            type="text"
+            value={formState.breed}
+            onChange={handleChange}
+          />
+          <input
+            className="form-input"
+            placeholder="Pet Gender"
+            name="sex"
+            type="text"
+            value={formState.sex}
+            onChange={handleChange}
+          />
+          <input
+            className="form-input"
+            placeholder="Pet Age"
+            name="age"
+            type="text"
+            value={formState.age}
+            onChange={handleChange}
+          />
+          <input
+            className="form-input"
+            placeholder="Pet Weight"
+            name="weight"
+            type="text"
+            value={formState.weight}
+            onChange={handleChange}
+          />
+          <button
+            className="btn btn-block btn-info"
+            style={{ cursor: "pointer" }}
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PetForm;
