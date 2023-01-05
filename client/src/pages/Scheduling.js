4ada051be49f389cc90dca31fb0678769d17ad7f
ABoss <ashleyjeanbostrom@gmail.com>
2023-01-04 19:28:51 -0700

    working scheduling page

--- client/src/pages/Scheduling.js
+++ client/src/pages/Scheduling.js
@@ -1,28 +1,19 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
-// import dayjs from 'dayjs';
-// import TextField from '@mui/material/TextField';
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-// import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import DateTimePicker from 'react-datetime-picker';
+import Header from '../components/Header';
 
-// export default function BasicDateTimePicker() {
-//   const [value, setValue] = React.useState(dayjs('2022-04-07'));
+// function for Scheduling page
+// function Scheduling() {
+//   const [value, onChange] = useState(new Date());
 
 //   return (
-//     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//       <DateTimePicker
-//         renderInput={(props) => <TextField {...props} />}
-//         label="DateTimePicker"
-//         value={value}
-//         onChange={(newValue) => {
-//           setValue(newValue);
-//         }}
-//       />
-//     </LocalizationProvider>
+//     <div>
+//       <h1>Scheduling</h1>
+//       <DateTimePicker />
+//     </div>
 //   );
 // }
-   
 
 // import ProfileList from '../components/ProfileList';
 
@@ -31,19 +22,47 @@ import { QUERY_PROFILES } from '../utils/queries';
 const Scheduling = () => {
   const { loading, data } = useQuery(QUERY_PROFILES);
   const profiles = data?.profiles || [];
-  const [value, onChange] = useState(new Date());
 
-  return (
-
+return (
+  <div>
+     <Header></Header>
 <div className='homeBody'>
     <h1>Schedule An Appointment</h1>
-    <div className='calendar'>
+  
         
-        <h2>This Week</h2>
-        <div className='week'></div>
-
         <form className='makeApt'>
-        <DateTimePicker />
+        <label>
+    Pet's Name:
+    <input type="text" name="name" />
+  </label>
+  <select id='day'>
+  <option value="mon">Monday</option>
+  <option value="tues">Tuesday</option>
+  <option selected value="wed">Wednesday</option>
+  <option value="thurs">Thursday</option>
+  <option value="fri">Friday</option>
+</select>
+<select id='time'>
+  <option value="9-11">9 AM - 11 AM</option>
+  <option value="11-1">11 AM - 1 PM</option>
+  <option selected value="1-3">1 PM - 3 PM</option>
+  <option value="3-5">3 PM - 5 PM</option>
+  <option value="5-7">5 PM - 7 PM</option>
+</select>
+<select id='service'>
+  {/* TODO @ ashley map through services and move button if you want */}
+  <option value="bathAndBrush">Bath And Brush</option>
+  <option value="nailFileAndPrep">Nail File and Prep</option>
+  <option selected value="topCat">Top Cat</option>
+  <option value="topCatPlus">Top Cat Plus</option>
+  <option value="bathAndClip">Bath And Clip</option>
+  <option value="nailGrindingAndSanitation">Nail Grinding & Sanitation</option>
+  <option value="topDog">Top Dog</option>
+  <option value="topDogPlus">Top Dog Plus</option>
+  <input type="submit" value="Submit" />
+
+</select>
+
             <select>
               {/* // day, time, and service */}
               
@@ -57,10 +76,9 @@ const Scheduling = () => {
 
         </form>
     
-    </div>
-    </div>
-  );
-                };
-
+  
+</div>
+</div>
+)};
 
 export default Scheduling;
