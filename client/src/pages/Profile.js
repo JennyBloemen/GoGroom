c2c022c48b09dfaa7436a2ca11f03753991970dc
Emmalee <emilee.wimmer@gmail.com>
2023-01-04 20:02:43 -0700

    merge conflicts resolved on profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -23,19 +23,15 @@ function Profile() {
   return (
     <div>
       <Header></Header>
-      <div className="homeBody">
-<<<<<<< HEAD
-        {/* <div className="userCard">
-          <h2>User's name</h2>
-=======
+      <div className="homeBody">        
         <div className="userCard">
           <h2>{user.data.name}</h2>
->>>>>>> origin
+
           <ul>
             <li>phone #:</li>
             <li>email:</li>
           </ul>
-        </div> */}
+        </div> 
         <div className="petCard">
           <img src="" alt="your pet here" />
           <h2>pet's name:</h2>
